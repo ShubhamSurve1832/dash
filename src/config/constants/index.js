844bbcdaec387df2fc173da424fe238b29a4481e
shubham surve <shubhamsurve1832@gmail.com>
2024-09-24 22:32:37 +0530

    container and config added
    container and config added

---
+++ src/config/constants/index.js
@@ -0,0 +1,26 @@
+
+export const FORMATS = ['jpg', 'png', 'bmp', 'jpeg', 'tiff', 'pdf', 'doc', 'docx'];
+export const DEFAULT_MAX_FILE_SIZE_IN_BYTES = 2000000;
+export const NAVIGATION = [
+    {
+        href: "/profile",
+        name: "Profile",
+    },
+    {
+        href: "/personal",
+        name: "Personal",
+    },
+    {
+        href: "/notification",
+        name: "Notifications",
+    },
+    {
+        href: "/wishlist",
+        name: "Wishlist",
+    },
+    {
+        href: "/help",
+        name: "Help",
+    },
+
+]
---
+++ src/config/constants/initialValues.js
@@ -0,0 +1,9 @@
+export const personalVals = {firstName: "",lastName: "",email: "",mobileNumber: "",dateOfBirth: "", ssn: "",driverLicenseNumber: "",driverLicenseState: "",streetAddress: "", city: "",  state:"", zipCode:""};
+
+export const employmentVals = {employer: "",occupation: "",employmentType: "",hourlyPay: "",hoursPerWeek: "",biWeeklyIncome: "",monthlyIncome: "",annualIncome: "",employmentStartDate: "",employmentDurationYears: "",employmentDurationMonths: "",residentType: "",residentDuration: "",rentAmount: "",yourShareOfRent: "",numberOfPeopleOnLease: "",additionalPeopleOnRent: "",sort:""};
+
+export const autoLoanVals = { requestedMonthlyPayment: "", requestedDownPayment: "", interestRate: "", loanTermMonths: "", ficoScore: "", remainingLoanBalance: "", monthlyPayment: "", remainingNumberOfPayments: "", currentInterestRate: "", vin: "", licensePlate: "", year: "", make: "", model: "", trim: "", currentMiles: "", notesOnCarCondition: "",carMikes:"", uploadVIN:"",uploadDamages:""};
+
+export const searchVehicle = {brand:"",modal:"",bodyStyle:"",year:""}
+
+export const FinalChecklistVals ={singedLoan:"", carFax:"",}
